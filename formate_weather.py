24b6f899457efdd37b525a9387cf49357929a0f9
M1stg4le <darplay0@gmail.com>
2025-06-27 07:13:25 +0300

    add emoji

--- formate_weather.py
+++ formate_weather.py
@@ -1,10 +1,17 @@
 def format_weather(weather: tuple) -> str:
     temperature, wind_speed, weather_type, sunset, sunrise = weather
-    emoji = []
+    weather_emojis = {
+    "Ясно": "☀️",
+    "Облачно": "☁️",
+    "Дождь": "🌧️",
+    "Снег": "❄️"
+}
+    emoji = weather_emojis.get(weather_type, "")
+
     return (
         f"Temperature: {temperature}°C\n"
         f"Wind Speed: {wind_speed} m/s\n"
-        f"Weather: {weather_type}\n"
+        f"Weather: {weather_type} {emoji}\n"
         f"Sunset: {sunset.strftime('%H:%M:%S')}\n"
         f"Sunrise: {sunrise.strftime('%H:%M:%S')}"
     )
